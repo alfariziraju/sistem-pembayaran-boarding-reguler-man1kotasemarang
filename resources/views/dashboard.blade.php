9add4e021f7d84a7254cb9510ccd33330905922d
alfariziraju <alfarafala15@gmail.com>
2025-02-15 18:50:29 +0700

    edit layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
@@ -15,3 +16,6 @@
         </div>
     </div>
 </x-app-layout>
+
+
+  
